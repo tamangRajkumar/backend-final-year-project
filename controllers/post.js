15289c12561c0c6cfa0beb82ca482b4d31c6358d
Rajkumar <rajkumartamang182@gmail.com>
2025-09-17 00:49:16 +0545

    fix: start issue

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,5 @@
-import Post from "../models/post";
-import User from "../models/user";
+import Post from "../models/post.js";
+import User from "../models/user.js";
 import cloudinary from "cloudinary";
 console.log("cloudinary key", process.env.CLOUDINARY_NAME);
 
